b5df01ab75a18fc9841eb7da6b9f75cc55ffa994
Tia <tia.paassilta-kalpio@student.laurea.fi>
2026-05-18 06:34:39 +0300

    commit

---
+++ stylelint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: "stylelint-config-standard",
+  rules: {
+    "media-feature-range-notation": null,
+    "no-duplicate-selectors": null,
+    "color-function-alias-notation": null,
+    "color-function-notation": null,
+    "alpha-value-notation": null,
+    "declaration-empty-line-before": null,
+    "rule-empty-line-before": null,
+    "color-hex-length": null,
+    "no-descending-specificity": null,
+    "font-family-name-quotes": null,
+    "shorthand-property-no-redundant-values": null,
+    "property-no-deprecated": null
+  }
+};
